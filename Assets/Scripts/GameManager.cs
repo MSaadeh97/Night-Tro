6fb0559472ad26a5ff0535d66d767883419d9efd
TimxK5 <59641989+TimxK5@users.noreply.github.com>
2021-02-10 10:51:48 -0500

    Code(Partial)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static int Level = 0;
+
+    public static int lives = 3;
+    public List<Image> livesImage = new List<Image>(3);
+
+    public Text scoreText;
+    static public int score;
+
+    public enum GameState { Init, Game, Dead, Scores }
+    public static GameState gameState;
+
+    private GameObject knightro;
+    private GameObject blinky;
+    private GameObject pinky;
+    private GameObject inky;
+    private GameObject clyde;
+
+
+    public static bool scared;
+    public float scareLength;
+    private float _timeToCalm;
+
+    public float ghostSpeed;
+
+    private static GameManager _instance;
+
+    public GameObject GM;
+
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+               // _instance = GameObject.FindObjectsOfType<GameManager>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (this != _instance)
+                Destroy(this.gameObject);
+        }
+
+        Assign();
+    }
+
+    void Start()
+    {
+        gameState = GameState.Init;
+    }
+
+    void OnLevelWasLoaded()
+    {
+        if (Level == 0) lives = 3;
+
+        Debug.Log("Level " + Level + " Loaded!");
+        Assign();
+        ResetVars();
+
+
+        // Adjust Ghost variables!
+        //clyde.GetComponent<GhostMove>().speed += Level * ghostSpeed;
+        //blinky.GetComponent<GhostMove>().speed += Level * ghostSpeed;
+        //pinky.GetComponent<GhostMove>().speed += Level * ghostSpeed;
+        //inky.GetComponent<GhostMove>().speed += Level * ghostSpeed;
+        knightro.GetComponent<PlayerController>().speed += Level * ghostSpeed / 2;
+    }
+
+    private void ResetVars()
+    {
+        _timeToCalm = 0.0f;
+        scared = false;
+        PlayerController.killstreak = 0;
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score\n" + score;
+
+        if (scared && _timeToCalm <= Time.time)
+        {
+            CalmGhosts();
+        }
+    }
+
+    public void ResetScene()
+    {
+        CalmGhosts();
+
+        knightro.transform.position = new Vector3(14.5f, 11f, 0f);
+        blinky.transform.position = new Vector3(15f, 20f, 0f);
+        pinky.transform.position = new Vector3(14.5f, 17f, 0f);
+        inky.transform.position = new Vector3(16.5f, 17f, 0f);
+        clyde.transform.position = new Vector3(12.5f, 17f, 0f);
+
+        knightro.GetComponent<PlayerController>().ResetDestination();
+        //blinky.GetComponent<GhostMove>().InitializeGhost();
+        //pinky.GetComponent<GhostMove>().InitializeGhost();
+        //inky.GetComponent<GhostMove>().InitializeGhost();
+        //clyde.GetComponent<GhostMove>().InitializeGhost();
+
+        gameState = GameState.Init;
+
+
+    }
+
+
+    public void ToggleScare()
+    {
+        if (!scared) ScareGhosts();
+        else CalmGhosts();
+    }
+    public void ScareGhosts()
+    {
+        scared = true;
+        //blinky.GetComponent<GhostMove>().Frighten();
+        //pinky.GetComponent<GhostMove>().Frighten();
+        //inky.GetComponent<GhostMove>().Frighten();
+        //clyde.GetComponent<GhostMove>().Frighten();
+        _timeToCalm = Time.time + scareLength;
+    }
+    public void CalmGhosts()
+    {
+        scared = false;
+        //blinky.GetComponent<GhostMove>().Calm();
+        //pinky.GetComponent<GhostMove>().Calm();
+        //inky.GetComponent<GhostMove>().Calm();
+        //clyde.GetComponent<GhostMove>().Calm();
+        PlayerController.killstreak = 0;
+    }
+    void Assign()
+    {
+        // find and assign ghosts
+        clyde = GameObject.Find("clyde");
+        pinky = GameObject.Find("pinky");
+        inky = GameObject.Find("inky");
+        blinky = GameObject.Find("blinky");
+        knightro = GameObject.Find("knightro");
+
+
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+        gameState = GameState.Dead;
+
+        //UIScript ui = GameObject.FindObjectOfType<UIScript>();
+        //Destroy(ui.lives[ui.lives.Count - 1]);
+        //ui.lives.RemoveAt(ui.lives.Count - 1);
+    }
+
+    public static void DestroySelf()
+    {
+
+        score = 0;
+        Level = 0;
+        lives = 3;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = .4f;
+
+    Vector2 dir = Vector2.zero;
+    Vector2 dest = Vector2.zero;
+    Vector2 queueDir = Vector2.zero;
+
+    public bool isDead = false;
+    public bool isVictory = false;
+
+    public static int killstreak = 0;
+
+    private GameManager GM;
+    //private ScoreManager SM;
+
+    [Serializable]
+    public class PointSprites
+    {
+        public GameObject[] pointSprites;
+    }
+
+    public PointSprites points;
+    void Start()
+    {
+        GM = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        //SM = GameObject.Find("Game Manager").GetComponent<ScoreManager>();
+        dest = transform.position;
+    }
+
+
+    void FixedUpdate()
+    {
+        switch (GameManager.gameState)
+        {
+            case GameManager.GameState.Game:
+                UserInput();
+                Animate();
+                break;
+
+/*            case GameManager.GameState.Dead:
+                if (!_deadPlaying)
+                    StartCoroutine("PlayDeathAnimation");
+                break;
+*/
+        }
+    }
+
+    void Animate()
+    {
+        Vector2 direction = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", direction.x);
+        GetComponent<Animator>().SetFloat("DirY", direction.y);
+    }
+
+    bool Valid(Vector2 direction)
+    {
+        Vector2 pos = transform.position;
+        direction += new Vector2(direction.x * 0.45f, direction.y * 0.45f);
+        RaycastHit2D hit = Physics2D.Linecast(pos + direction, pos);
+        return hit.collider.name == "pellet" || (hit.collider == GetComponent<Collider2D>());
+    }
+
+    public void ResetDestination()
+    {
+        dest = new Vector2(14.5f, 11f);
+        GetComponent<Animator>().SetFloat("DirX", 1);
+        GetComponent<Animator>().SetFloat("DirY", 0);
+    }
+
+    void UserInput()
+    {
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        if (Input.GetAxis("Horizontal") > 0) queueDir = Vector2.right;
+        if (Input.GetAxis("Horizontal") < 0) queueDir = Vector2.left;
+        if (Input.GetAxis("Vertical") > 0) queueDir = Vector2.up;
+        if (Input.GetAxis("Vertical") < 0) queueDir = Vector2.down;
+
+        if (Vector2.Distance(dest, transform.position) < 0.00001f)
+        {
+            if (Valid(queueDir))
+            {
+                dest = (Vector2)transform.position + queueDir;
+                dir = queueDir;
+            }
+            else   // if next direction is not valid
+            {
+                if (Valid(dir))
+                    dest = (Vector2)transform.position + dir;
+            }
+        }
+    }
+
+    public Vector2 getDir()
+    {
+        return dir;
+    }
+
+    public void UpdateScore()
+    {
+        killstreak++;
+
+        // limit killstreak at 4
+        if (killstreak > 4) killstreak = 4;
+
+        Instantiate(points.pointSprites[killstreak - 1], transform.position, Quaternion.identity);
+        GameManager.score += (int)Mathf.Pow(2, killstreak) * 100;
+
+    }
+
+}
+
